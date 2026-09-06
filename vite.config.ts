0f9fa1c9ac4932678d51c12a133d8d086e7203cb
gn00435942 <gn00435942@gmail.com>
2026-09-06 23:29:39 +0800

    build: publish integrated app under preview path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/class/',
+  base: '/class/next/',
   plugins: [react()],
   resolve: {
     alias: {
